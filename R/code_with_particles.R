31f18216885377b1648b59c775d4a5f5708411a3
Maëlle Salmon <maelle.salmon@yahoo.se>
2018-03-27 13:00:59 +0200

    A SQUARE :tada: and COMMENTS :angel:

--- R/code_with_particles.R
+++ R/code_with_particles.R
@@ -12,21 +12,7 @@ set.seed("20180401")
 winner <- dplyr::sample_n(followers, size = 1) %>%
   rtweet::lookup_users()
 
-plot_fun <- function(sim, winner) {
-  df <- tibble::as_tibble(sim)
-  par(bg = "#2165B6", family = "contrail", cex = 2)
-  plot(c(min(df$x), max(df$x)),
-       c(min(df$y), max(df$y)),
-       col = "#2165B6", axes = FALSE,
-       xlim = c(-100, 317), ylim = c(-268, 100), xlab = NA, ylab = NA)
-  legend(-25, 0, legend = paste0(winner$name, "\n(@",
-                                winner$screen_name, ")!"),
-         box.lwd = 0,box.col = "#2165B6",bg = "#2165B6",
-         text.col = "white")
-  points(df$x, df$y, col = df$color, pch = '.', axes = FALSE,
-         xlim = c(-100, 317), ylim = c(-268, 100), xlab = NA, ylab = NA)
-}
-
+# prepare the book image
 book <- magick::image_read(deardata_url) %>%
   magick::image_scale('55%')
 
@@ -40,6 +26,27 @@ book_frame <- reshape2::melt(as.matrix(as.raster(book)), c('y', 'x'),
          y_drift = rnorm(n(), 300, 70),
          x_drift = rnorm(n(), 300, 90))
 
+# function plotting each simulation
+# only tweaks compared to Thomas' code:
+# blue background and name of the winner under the book
+plot_fun <- function(sim, winner) {
+  df <- tibble::as_tibble(sim)
+  par(bg = "#2165B6", family = "contrail", cex = 1.7)
+  plot(c(min(df$x), max(df$x)),
+       c(min(df$y), max(df$y)),
+       col = "#2165B6", axes = FALSE,
+       xlim = c(-100, 317), ylim = c(-268, 100), xlab = NA, ylab = NA)
+  legend(-52, 0, legend = paste0(winner$name, "\n(@",
+                                 winner$screen_name, ")!"),
+         box.lwd = 0,box.col = "#2165B6",bg = "#2165B6",
+         text.col = "white")
+  points(df$x, df$y, col = df$color, pch = '.', axes = FALSE,
+         xlim = c(-100, 317), ylim = c(-268, 100), xlab = NA, ylab = NA)
+}
+
+# this piece of code creates a gif
+# the expression producing plots uses tbl_graph and particles
+# to make pixels move
 saveGIF(
   tbl_graph(book_frame) %>%
     simulate(alpha_decay = 0, setup = predefined_genesis(x, y)) %>%
@@ -54,18 +61,20 @@ saveGIF(
       sim
     }),
   movie.name = 'deardata.gif',
-  interval = 1/24, ani.width = 594, ani.height = 822
+  interval = 1/24
 )
 
 
 # now add chibi
+# I have tried a bit to add it directly
+# via using magick drawing/graphic device but wasn't too successful
+# it is a pity because here the image_write takes aaages
 chibi <- magick::image_read("assets/wizard_steph.png") %>%
   magick::image_resize("230x230")
-gif <- magick::image_read('deardata.gif')
-gif_with_chibi <- magick::image_composite(gif, chibi,
-                                          offset = "+5+550")
 
-magick::image_animate(gif_with_chibi, fps = 10) %>%
+magick::image_read('deardata.gif') %>%
+  magick::image_composite(chibi, offset = "+5+550") %>%
+  magick::image_animate(gif_with_chibi, fps = 10) %>%
   magick::image_write(final_gif, "tada.gif")
 
 file.remove("deardata.gif")
