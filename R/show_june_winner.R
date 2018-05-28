054dc594946daa4b364cee5ea70bb4074c094ac3
Maëlle Salmon <maelle@itsalocke.com>
2018-05-28 16:28:55 +0200

    add comments

--- R/show_june_winner.R
+++ R/show_june_winner.R
@@ -26,50 +26,61 @@ show_june_winner <- function(winner, path = "june.gif"){
 }
 
 june_background <- function(){
+  # create a blue rectangle as background
   background <-  magick::image_blank(400, 300, '#2165B6')
 
-
+  # activate it as a plot background
   img <- magick::image_draw(background)
-  # add stars
+  # add stars using base plot!
   points(runif(n = 42, min = 0, max = 400),
          runif(n = 42, min = 0, max = 300),
          cex = runif(42, min = 2, max = 4),
          col = "white",
          pch = 8)
 
-  # add ball thing
+  # add ball support using base plot as well
   rect(250, 250, 350, 150,
        border = "white", lwd = 5,
        col = "white")
 
-  # add ball
+  # add crystal ball, its background is a random color
   symbols(300, 150, circles = 50,
           fg = "white", inches = 1, add = TRUE,
           bg = "#B657A3", lwd = 2)
 
   dev.off()
 
-  # wizard chibi
+  # get and resize wizard chibi
   chibi <- magick::image_read(system.file("assets/wizard_steph.png",
                                           package="twitterbookdraw")) %>%
     magick::image_resize("200x200")
 
 
-  # add chibi to background
+  # add chibi on background
   img <- magick::image_composite(img, chibi, offset = "+10+50",
                                  operator = "Over")
+
+  # make the crystal ball transparent by replacing its color
+  # by transparency. the hole is done!
   img <- magick::image_transparent(img, "#B657A3")
+
+  # return face-in-hole photo prop
   img
 }
 
 june_colorized_frame <- function(opacity, winner_face, background){
 
+  # add opacity veil in front of the winner's avatar
   winner_face <- winner_face %>%
     magick::image_colorize(opacity = opacity,
                            color = "black")
 
+  # put the winner's avatar on a white rectangle
+  # which will help put the avatar right behind the hole
   winner_face <- magick::image_blank(400, 300, "white") %>%
     magick::image_composite(winner_face, offset = "+220+80")
 
+  # put the carefully placed and veiled winner's avatar
+  # behind the photo prop
   magick::image_composite(winner_face, background)
 }
